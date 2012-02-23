5a3d20d501f2b844f393ad14b05fac514904563d
ben.franksen <ben.franksen@online.de>
2012-02-23 17:16:16 +0000

    seq: fix resource leak when something fails (pvAssign, seq_connect)

--- src/seq/seq_if.c
+++ src/seq/seq_if.c
@@ -565,6 +565,7 @@ epicsShareFunc pvStat epicsShareAPI seq_pvAssign(SS_ID ss, VAR_ID varId, const c
 		if (!dbch->dbName)
 		{
 			errlogSevPrintf(errlogFatal, "pvAssign: epicsStrDup failed\n");
+			free(dbch);
 			return pvStatERROR;
 		}
 		if ((sp->options & OPT_SAFE) && sp->numSS > 0)
@@ -573,6 +574,8 @@ epicsShareFunc pvStat epicsShareAPI seq_pvAssign(SS_ID ss, VAR_ID varId, const c
 			if (!dbch->ssMetaData)
 			{
 				errlogSevPrintf(errlogFatal, "pvAssign: calloc failed\n");
+				free(dbch->dbName);
+				free(dbch);
 				return pvStatERROR;
 			}
 		}
@@ -589,6 +592,10 @@ epicsShareFunc pvStat epicsShareAPI seq_pvAssign(SS_ID ss, VAR_ID varId, const c
 		{
 			errlogSevPrintf(errlogFatal, "pvAssign: pvVarCreate() %s failure: "
 				"%s\n", dbch->dbName, pvVarGetMess(dbch->pvid));
+			if (ch->dbch->ssMetaData)
+				free(ch->dbch->ssMetaData);
+			free(ch->dbch->dbName);
+			free(ch->dbch);
 		}
 	}
 
